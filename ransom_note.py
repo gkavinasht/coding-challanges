3aa3a9373105108a8d7831bc8912e319703c7234
Avinash <gkavinasht@gmail.com>
2023-06-01 21:22:45 -0700

    add ransom_note.py

---
+++ ransom_note.py
@@ -0,0 +1,52 @@
+# 383. Ransom Note
+
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+# Each letter in magazine can only be used once in ransomNote.
+
+def canConstruct(ransomNote, magazine):
+	magazineDict = {}
+
+	for i in range(len(magazine)):
+	    if magazine[i] in magazineDict:
+	        magazineDict[magazine[i]] += 1
+	    else:
+	        magazineDict[magazine[i]] = 1
+
+	for i in range(len(ransomNote)):
+	    if ransomNote[i] not in magazineDict:
+	        return False
+	    if magazineDict[ransomNote[i]] == 0:
+	        return False
+	    magazineDict[ransomNote[i]] -= 1
+	return True
+
+    # ransomNoteDict = {}
+    # magazineDict = {}
+
+    # for i in range(len(magazine)):
+    #     if magazine[i] in magazineDict:
+    #         magazineDict[magazine[i]] += 1
+    #     else:
+    #         magazineDict[magazine[i]] = 1
+
+    # for i in range(len(ransomNote)):
+    #     if ransomNote[i] in ransomNoteDict:
+    #         ransomNoteDict[ransomNote[i]] += 1
+    #     else:
+    #         ransomNoteDict[ransomNote[i]] = 1
+
+    # for key in ransomNoteDict.keys():
+    #     if key not in magazineDict:
+    #         return False
+    #     if magazineDict[key] < ransomNoteDict[key]:
+    #         return False
+    # return True
+
+
+ransomNote = "a"
+magazine = "ab"
+print(canConstruct(ransomNote, magazine))
+# Output: true
+
+# Time Complexity : O(n)
+# Space Complexity : O(k) Space to store unique elements in the Dictionary 
