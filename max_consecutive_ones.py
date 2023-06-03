d9231a757be3ca69b54a076845141554dc9f89c3
Avinash <gkavinasht@gmail.com>
2023-06-02 19:10:10 -0700

    add max_consecutive_ones.py

--- max_consecutive_ones.py
+++ max_consecutive_ones.py
@@ -2,3 +2,22 @@
 
 # Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
+def findMaxConsecutiveOnes(nums):
+    maxFrequency = frequency = 0
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            frequency += 1
+        else:
+            maxFrequency = max(frequency, maxFrequency)
+            frequency = 0
+    maxFrequency = max(frequency, maxFrequency)
+    return maxFrequency
+
+nums = [1,1,0,1,1,1]
+print(findMaxConsecutiveOnes(nums))
+
+# Output: 3
+# Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+# Time Complexity : O(n)
+# Space Complexity: O(1)
