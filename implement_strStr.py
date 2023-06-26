d3c62a64c6e6caeefba54a22c32cd893d52cbb37
Avinash <gkavinasht@gmail.com>
2023-06-25 20:15:23 -0700

    add implement_strStr.py

---
+++ implement_strStr.py
@@ -0,0 +1,34 @@
+# Implement strStr()
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+def strStr(haystack, needle):
+    if needle == "":
+        return 0
+    
+    if len(haystack) < len(needle):
+        return -1
+    
+    i, j, index = 0, 0, 0
+    while i < len(haystack) and j < len(needle):
+        if haystack[i] == needle[j]:
+            i += 1
+            j += 1
+        else:
+            i = index + 1
+            j = 0
+            index = i
+            
+    return index if j == len(needle) else -1
+
+    # return haystack.find(needle)
+
+haystack = "sadbutsad"
+needle = "sad"
+print(strStr(haystack, needle))
+
+# Time Complexity: O(m * n)
+# Space Complexity: O(1)
+
+# Output: 0
+# Explanation: "sad" occurs at index 0 and 6.
+# The first occurrence is at index 0, so we return 0.
