84f37c171391e713b4d4ac995f5ca3c466e0a66a
Avinash <gkavinasht@gmail.com>
2023-07-07 19:07:39 -0700

    add single_number.py

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ The below Leetcode coding challanges are implemented in Python programming langu
 92. Reverse Linked List II
 118. Pascal's Triangle
 119. Pascal's Triangle II
+136. Single Number
 138. Copy List with Random Pointer
 141. Linked List Cycle
 142. Linked List Cycle II
---
+++ single_number.py
@@ -0,0 +1,28 @@
+136. Single Number
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+def singleNumber(nums):
+    a = 0
+    for num in nums:
+        a ^= num
+    return a
+
+    # Time Complexity: O(n)
+	# Space Complexity: O(1)
+    
+    # numDict = {}
+    # for num in nums:
+    #     numDict[num] = numDict.get(num, 0) + 1
+
+    # for key, val in numDict.items():
+    #     if val == 1:
+    #         return key
+
+    # Time Complexity: O(n)
+	# Space Complexity: O(n)
+
+nums = [2,2,1]
+print(singleNumber(nums))
+
+Output: 1
