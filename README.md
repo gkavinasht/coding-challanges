23dd02f893e53103bb06fdb136de5a8fd2efd721
Avinash <gkavinasht@gmail.com>
2023-07-10 19:41:04 -0700

    add find_duplicate_subtrees.py

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ The below Leetcode coding challanges are implemented in Python programming langu
 ```
 1. Two Sum
 2. Add Two Numbers
+3. Longest Substring Without Repeating Characters
 14. Longest Common Prefix
 19. Remove Nth Node From End of List
 21. Merge Two Sorted Lists
--- jewels_and_stones.py
+++ jewels_and_stones.py
@@ -2,6 +2,18 @@
 # You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
 # Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
+def numJewelsInStones(jewels, stones):
+    res = 0
+    for stone in stones:
+        if stone in jewels:
+            res += 1
+    return res
+
 jewels = "aA"
 stones = "aAAbbbb"
+print(numJewelsInStones(jewels, stones))
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
 # Output: 3
