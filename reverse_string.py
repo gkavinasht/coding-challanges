84057d52fbd8778105c74eae398a06cbfe12abb2
Avinash <gkavinasht@gmail.com>
2023-07-31 18:59:21 -0700

    add recursion

--- reverse_string.py
+++ reverse_string.py
@@ -6,17 +6,31 @@ def reverseString(s):
     """
     Do not return anything, modify s in-place instead.
     """
+    # Iterative
     i, j = 0, len(s) - 1
     while i < j:
         s[i], s[j] = s[j], s[i]
         i += 1
         j -= 1
 
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+
+    # Recursion
+    # def helper(i, j):
+    #     if i >= j:
+    #         return
+            
+    #     s[i], s[j] = s[j], s[i]
+    #     helper(i + 1, j - 1)
+
+    # helper(0, len(s) - 1)
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+
 s = ["h","e","l","l","o"]
 reverseString(s)
 print(s)
-
-# Time Complexity: O(n)
-# Space Complexity: O(1)
 
 # Output: ["o","l","l","e","h"]
