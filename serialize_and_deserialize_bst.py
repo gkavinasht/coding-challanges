6928d032dca790a38b27bfb4eb924cfd240e9642
Avinash <gkavinasht@gmail.com>
2023-09-22 22:31:38 -0700

    add serialize_and_deserialize_bst.py

--- serialize_and_deserialize_bst.py
+++ serialize_and_deserialize_bst.py
@@ -20,32 +20,31 @@ class Codec:
         """
     ....Encodes a tree to a single string.
         """
-        res = []
-        def inorder(root):
-            if root:
-                inorder(root.left)
-                res.append(str(root.val))
-                inorder(root.right)
-
-            return res
-
-        inorder(root)
-        return ' '.join(res)
+        output = []
+        def postorder(root):
+            if root is None:
+                return []
+            
+            postorder(root.left)
+            postorder(root.right)
+            output.append(str(root.val))
+            
+            return output
+        return ' '.join(postorder(root))
 
     def deserialize(self, data):
-        def constructTree(data):
-            if not data:
+        def helper(lower = float('-inf'), upper = float('inf')):
+            if not data or data[-1] < lower or data[-1] > upper:
                 return None
-                
-            ind = len(data) // 2
-            root = TreeNode(data[ind])
-            root.left = constructTree(data[:ind])
-            root.right = constructTree(data[ind + 1:])
-
-            return root
             
+            val = data.pop()
+            root = TreeNode(val)
+            root.right = helper(val, upper)
+            root.left = helper(lower, val)
+            return root
+        
         data = [int(x) for x in data.split(' ') if x]
-        return constructTree(data)
+        return helper()
 
 # root = [2,1,3]
 
